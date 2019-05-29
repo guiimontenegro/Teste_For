e4e86008fa2c3641e9e1e6a1f698eb2fc38d7296
guiimontenegro <guiimontenegro@gmail.com>
2019-05-29 14:17:40 -0300

    Adicionar arquivos de projeto.

---
+++ Teste_For/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Teste_For
+{
+    public partial class btnTeste : Form
+    {
+        public btnTeste()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0;  i < 50;  i++)
+            {
+                MessageBox.Show("O valor do contador é: ", + i);
+            }
+        }
+    }
+}
